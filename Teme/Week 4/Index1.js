3ac52adf9fb8fff985c528c3b0af7eba64165b89
Catalin Mandru <mandruvalentin96@yahoo.com>
2022-09-23 18:09:25 +0300

    Tema completa

--- Teme/Week 4/Index1.js
+++ Teme/Week 4/Index1.js
@@ -67,3 +67,41 @@ console.log(myarray2[0]);
 console.log(myarray2[1]);
 console.log(myarray2[2]);
 console.log(myarray2[3]);
+
+//Second Ex
+//1
+
+console.log("BMI")
+
+var myarray3 = [
+    {
+        Name: "John Doe",
+        Gender: "Male",
+        Height: 1.69,
+        Weight: 70
+    },
+    {
+        Name: "Mary Anne",
+        Gender: "Female",
+        Height: 1.63,
+        Weight: 100
+    }];
+
+const BMI = (x,y) => {
+    return(x / (y*y));
+}
+
+for(var n in myarray3){
+    if(BMI(myarray3[n].Weight, myarray3[n].Height) < 18.5){
+        console.log(myarray3[n].Name + " is underweight because his BMI is " + BMI(myarray3[n].Weight, myarray3[n].Height));    
+    }
+    if(BMI(myarray3[n].Weight, myarray3[n].Height) < 25 && BMI(myarray3[n].Weight, myarray3[n].Height) >= 18.5){
+        console.log(myarray3[n].Name + " is normal because his BMI is " + BMI(myarray3[n].Weight, myarray3[n].Height));
+    }
+    if(BMI(myarray3[n].Weight, myarray3[n].Height) < 30 && BMI(myarray3[n].Weight, myarray3[n].Height) >= 25){
+        console.log(myarray3[n].Name + " is overweight because his BMI is " + BMI(myarray3[n].Weight, myarray3[n].Height));
+    }
+    if(BMI(myarray3[n].Weight, myarray3[n].Height) >= 30){
+        console.log(myarray3[n].Name + " is obese because his BMI is " + BMI(myarray3[n].Weight, myarray3[n].Height));
+    }
+}
